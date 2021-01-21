29bcfe8155289162701fff8a196d774985cb4983
Matthew Elmore <elmo5676@gmail.com>
2021-01-20 17:51:23 -0800

    Building Frameworks and SOF

--- Sources/CIAeronautical/Protocols/Approach.swift
+++ Sources/CIAeronautical/Protocols/Approach.swift
@@ -8,9 +8,46 @@
 
 import Foundation
 
-public enum ApproachCategory {
-    case a, b, c, d, e
+public enum TermAppType: String, CaseIterable {
+    
+    case APPROACH_TRANSITION = "A"
+    case ILS_BACK_COURSE = "B"
+    case ILS_CAT_II = "C"
+    case VOR_DME_VORTAC_BASED_ON_VORDME_OR_VORTAC = "D"
+    case VOR_CIRCLING_APPROACH = "E"
+    case NDB_CIRCLING_APPROACH = "F"
+    case RNAV = "G"
+    case ILS_CAT_III = "H"
+    case ILS = "I"
+    case LAAS_GPS = "J"
+    case WAAS_GPS = "K"
+    case ILS_LOCALIZER_ONLY_NO_GS = "L"
+    case MLS = "M"
+    case NDB = "N"
+    case RNAV_GPS_OVERLAY_APPROACH = "O"
+    case PAR = "P"
+    case NDB_DME = "Q"
+    case RNAV_RNP = "R"
+    case VOR_BASED_ON_VORDME_OR_VORTAC = "S"
+    case TACAN = "T"
+    case MLS_CAT_II = "U"
+    case VOR_NO_DME = "V"
+    case ADF = "W"
+    case LDA = "X"
+    case SDF = "Y"
+    case MLS_CAT_III = "Z"
+    case RNAV_GPS_PRECISION_APPROACH_OTHER = "1"
+    case ILS_LOCALIZER_ONLY_CIRCLING_APPROACH = "2"
+    case ILS_BACK_COURSE_CIRCLING_APPROACH = "3"
 }
+
+public enum ApproachCategory: CaseIterable {
+    case A, B, C, D, E
+}
+//
+//public enum ApproachCategory {
+//    case a, b, c, d, e
+//}
 
 public enum ApproachType {
     case precision, nonPrecision
