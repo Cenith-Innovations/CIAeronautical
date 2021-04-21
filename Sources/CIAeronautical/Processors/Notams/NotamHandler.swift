e56e1d37708f2b568d9ce794f901f3b58686f61e
Matthew Elmore <elmo5676@gmail.com>
2021-04-21 08:37:11 -0700

    Building

--- Sources/CIAeronautical/Processors/Notams/NotamHandler.swift
+++ Sources/CIAeronautical/Processors/Notams/NotamHandler.swift
@@ -31,10 +31,13 @@ public struct NotamHandler {
     public static func getAllClosedRunways(notam: String) -> [String] {
         var runways: [String] = []
         var n = removeNewLinesAndSpaces(notam: notam)
+        print(n)
         if let twyStart = n.range(of: "TWY")?.upperBound {
             if let clsdEnd = n.range(of: "CLSD")?.lowerBound{
-                let range = twyStart...clsdEnd
-                n.removeSubrange(range)
+                if twyStart < clsdEnd {
+                    let range = twyStart...clsdEnd
+                    n.removeSubrange(range)
+                }
             }}
         if let startIndex = n.range(of: "RWY")?.upperBound {
             if let endIndex = n.range(of: "CLSD")?.lowerBound {
