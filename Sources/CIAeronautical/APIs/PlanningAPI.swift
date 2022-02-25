364fe36b9d56cf9f03b2f46ff4a5e2f155cd69f1
Jorge Alvarez <jorgelalvarez52@gmail.com>
2022-02-25 10:50:51 -0800

    changes metar publisher for PlanningAPI back to an optional Metar

--- Sources/CIAeronautical/APIs/PlanningAPI.swift
+++ Sources/CIAeronautical/APIs/PlanningAPI.swift
@@ -33,7 +33,7 @@ public class PlanningAPI: ObservableObject {
     
     // MARK: - 🔅 Publishers: METARs
     /// Publisher that contains the METAR for the input ICAO.
-    @Published public var metar = Metar()
+    @Published public var metar: Metar?// = Metar()
 
     /// Temp Holder for metars to get rid of duplicates. The intended public Publisher is the Array of Metars
 //    @Published private var metarSet: Set<Metar> = []
@@ -52,7 +52,7 @@ public class PlanningAPI: ObservableObject {
     /// Clears all of the publishers.
     public func clearAll() {
         notams = [:]
-        metar = Metar()
+        metar = nil //Metar()
         tafs = []
     }
     
